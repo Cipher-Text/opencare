b776d5c8db11c345594a2d19f859ad210dbcb29e
Sadman Sobhan <imran110219@gmail.com>
2023-06-20 23:00:56 +0600

    remove unused frontend

--- medical-information-react/src/DoctorPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DoctorPage = () => {
-  return <h1>Welcome to Doctor Page</h1>;
-};
-
-export default DoctorPage;
--- medical-information-react/src/HospitalPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HospitalPage = () => {
-  return <h1>Welcome to Hospital Page</h1>;
-};
-
-export default HospitalPage;
