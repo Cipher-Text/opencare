9fadb2528e22c51dee9e195eb0087f89f7ae0881
Sadman Sobhan <imran110219@gmail.com>
2023-06-25 23:38:51 +0600

    change to english

--- open-care-frontend/src/pages/dashboard/monitor/components/ActiveChart/index.tsx
+++ open-care-frontend/src/pages/dashboard/monitor/components/ActiveChart/index.tsx
@@ -59,15 +59,15 @@ export default class ActiveChart extends Component {
 
     return (
       <div className={styles.activeChart}>
-        <Statistic title="目标评估" value="有望达到预期" />
+        <Statistic title="Target assessment" value="is expected to meet expectations" />
         <div style={{ marginTop: 32 }}>
           <TinyArea data={activeData} xField="x" forceFit yField="y" height={84} />
         </div>
         {activeData && (
           <div>
             <div className={styles.activeChartGrid}>
-              <p>{[...activeData].sort()[activeData.length - 1].y + 200} 亿元</p>
-              <p>{[...activeData].sort()[Math.floor(activeData.length / 2)].y} 亿元</p>
+              <p>{[...activeData].sort()[activeData.length - 1].y + 200} 100 million yuan</p>
+              <p>{[...activeData].sort()[Math.floor(activeData.length / 2)].y} 100 million yuan</p>
             </div>
             <div className={styles.dashedLine}>
               <div className={styles.line} />
