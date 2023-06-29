2858e92eb90f65edb499b76382a7dba423b13883
Sadman Sobhan <imran110219@gmail.com>
2023-06-29 21:44:13 +0600

    add institution csv

--- open-care-backend/src/main/java/com/ciphertext/opencarebackend/mappers/InstitutionMapper.java
+++ open-care-backend/src/main/java/com/ciphertext/opencarebackend/mappers/InstitutionMapper.java
@@ -13,6 +13,6 @@ import org.mapstruct.factory.Mappers;
 public interface InstitutionMapper {
     InstitutionMapper INSTANCE = Mappers.getMapper( InstitutionMapper.class );
 
-    @Mapping(source = "numberOfSeats", target = "seatCount")
+//    @Mapping(source = "numberOfSeats", target = "seatCount")
     InstitutionDTO institutionToInstitutionDTO(Institution institution);
 }
