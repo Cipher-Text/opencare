bc1495e2d424b88de75606f3feab459706ad6487
Sadman Sobhan <imran110219@gmail.com>
2023-06-30 23:49:57 +0600

    remove institution

--- open-care-frontend/src/locales/en-US/menu.ts
+++ open-care-frontend/src/locales/en-US/menu.ts
@@ -24,7 +24,6 @@ export default {
   'menu.list': 'List',
   'menu.list.table-list': 'Search Table',
   'menu.list.doctor-list': 'Doctor Table',
-  'menu.list.institution-list': 'Institution Table',
   'menu.list.basic-list': 'Basic List',
   'menu.list.card-list': 'Card List',
   'menu.list.search-list': 'Search List',
