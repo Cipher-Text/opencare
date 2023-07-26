341e34fa286325b211ae1b18133a8dd14564412b
Sadman Sobhan <imran110219@gmail.com>
2023-07-27 00:39:10 +0600

    add advice

--- open-care-backend/src/main/java/com/ciphertext/opencarebackend/controller/api/HospitalApiController.java
+++ open-care-backend/src/main/java/com/ciphertext/opencarebackend/controller/api/HospitalApiController.java
@@ -1,26 +1,20 @@
 package com.ciphertext.opencarebackend.controller.api;
 
-import com.ciphertext.opencarebackend.dto.out.HospitalDTO;
 import com.ciphertext.opencarebackend.exception.ResourceNotFoundException;
 import com.ciphertext.opencarebackend.iservice.HospitalService;
 import com.ciphertext.opencarebackend.model.Hospital;
 import com.ciphertext.opencarebackend.repository.HospitalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.data.domain.Sort.Order;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,7 +24,7 @@ import java.util.Map;
 @RequestMapping("/api")
 public class HospitalApiController {
     @Autowired
-    HospitalService service;
+    HospitalService hospitalService;
 
     @Autowired
     HospitalRepository hospitalRepository;
@@ -43,7 +37,7 @@ public class HospitalApiController {
             @RequestParam(required = false) Integer districtId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size) {
-        try {
+
             Pageable pagingSort = PageRequest.of(page, size);
             Page<Hospital> pageHospitals = hospitalRepository.getFilteredHospitals(name, bnName, numberOfBed, districtId, pagingSort);
 
@@ -54,31 +48,28 @@ public class HospitalApiController {
             response.put("totalPages", pageHospitals.getTotalPages());
 
             return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
     @GetMapping("/hospitals/{id}")
     public ResponseEntity<Hospital> getHospitalById(@PathVariable(value = "id") int hospitalId)
             throws ResourceNotFoundException {
-        Hospital hospital = service.getHospitalById(hospitalId);
+        Hospital hospital = hospitalService.getHospitalById(hospitalId);
         return ResponseEntity.ok().body(hospital);
     }
 
     @PostMapping("/hospitals")
     public Hospital createHospital(@Valid @RequestBody Hospital hospital) {
-        return service.createHospital(hospital);
+        return hospitalService.createHospital(hospital);
     }
 
     @PutMapping("/hospitals/edit/{id}")
     public Hospital editHospitalById(@RequestBody Hospital newHospital, @PathVariable(value = "id") int hospitalId) {
-        return service.updateHospital(newHospital, hospitalId);
+        return hospitalService.updateHospital(newHospital, hospitalId);
     }
 
     @DeleteMapping("/hospitals/delete/{id}")
     @ResponseBody
     public ResponseEntity<Object> deleteHospitalsById(@PathVariable(value = "id") int hospitalId) {
-        return service.deleteHospitalById(hospitalId);
+        return hospitalService.deleteHospitalById(hospitalId);
     }
 }
