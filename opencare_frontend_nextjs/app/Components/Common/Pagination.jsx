9c02e55d3b55b0d5341478e5ea2a777a3d05ead0
snehadevi <snehadevi2016105@gmail.com>
2023-08-12 22:40:30 +0600

    remove unused codeblock

--- opencare_frontend_nextjs/app/Components/Common/Pagination.jsx
+++ opencare_frontend_nextjs/app/Components/Common/Pagination.jsx
@@ -14,17 +14,6 @@ const Pagination = ({ currentPage, totalPages, onPageChange, size, onSizeChange 
               class="flex items-center justify-center w-10 h-10 text-indigo-600 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-indigo-100">
               <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path></svg></button>
             </li>
-            {/* {[...Array(currentPage < 3 ? currentPage : 3)].map((_, index) =>
-              <li><button onClick={() => onPageChange(currentPage > 3 ? currentPage - 3 + index : index)}
-                class="w-10 h-10 text-indigo-600 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-indigo-100">
-                {currentPage > 3 ? currentPage - 2 + index : index + 1}</button></li>
-            )}
-            {[...Array(...Array(currentPage + 1 > totalPages ? 0 : (currentPage < 3 ? Math.min(7 - currentPage, totalPages - currentPage) : Math.min(4, totalPages - currentPage))))].map((_, index) =>
-              <li><button onClick={() => onPageChange(index + currentPage)}
-                class={currentPage == (index + currentPage) ? "w-10 h-10 text-indigo-600 transition-colors duration-150 rounded-full focus:shadow-outline bg-indigo-300"
-                  : "w-10 h-10 text-indigo-600 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-indigo-100"}>{index + currentPage + 1}</button></li>
-            )} */}
-
             {
               totalPages < 7 ? (
                 // all buttons
