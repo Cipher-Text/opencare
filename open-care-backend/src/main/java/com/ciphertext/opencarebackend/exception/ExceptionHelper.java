9d6bd2bf23e20802069a722303917ad69380f92b
mazharibnazahur <mazhar.zahur@dohatec.com.bd>
2023-08-19 13:36:27 +0600

    add validator exception in global

--- open-care-backend/src/main/java/com/ciphertext/opencarebackend/exception/ExceptionHelper.java
+++ open-care-backend/src/main/java/com/ciphertext/opencarebackend/exception/ExceptionHelper.java
@@ -1,30 +1,37 @@
 package com.ciphertext.opencarebackend.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 /**
  * @author Sadman
  */
 @ControllerAdvice
+@Slf4j
 public class ExceptionHelper {
 
-    private static final Logger logger = LoggerFactory.getLogger(ExceptionHelper.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        logger.error("Record Not Found Exception: ", ex.getMessage());
+        log.error("Record Not Found Exception: ", ex.getMessage());
         ErrorMessage message = new ErrorMessage(
                 HttpStatus.NOT_FOUND.value(),
-                new Date(),
+                LocalDateTime.now(),
                 ex.getMessage(),
                 request.getDescription(false));
 
@@ -33,13 +40,34 @@ public class ExceptionHelper {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
-        logger.error("INTERNAL SERVER ERROR: ", ex.getMessage());
+        log.error("INTERNAL SERVER ERROR: ", ex.getMessage());
         ErrorMessage message = new ErrorMessage(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                new Date(),
+                LocalDateTime.now(),
                 ex.getMessage(),
                 request.getDescription(false));
 
         return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e, WebRequest request) {
+        BindingResult result = e.getBindingResult();
+        List<String> fieldValidationErrors = new ArrayList<>();
+
+        result
+                .getFieldErrors()
+                .forEach(
+                        fieldError ->
+                                fieldValidationErrors.add(
+                                        fieldError.getField() + " => " + fieldError.getDefaultMessage()));
+        return ErrorMessage.builder()
+                .timestamp(LocalDateTime.now())
+                .message(String.join("/n", fieldValidationErrors))
+                .description(request.getDescription(false))
+                .build();
+    }
+
 }
