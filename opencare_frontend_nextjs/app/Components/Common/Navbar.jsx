c1e8073736f2ccfed4336e760db502c5a5ca1884
snehadevi <snehadevi2016105@gmail.com>
2023-09-09 16:32:55 +0600

    use Link component and apply classes based on url

--- opencare_frontend_nextjs/app/Components/Common/Navbar.jsx
+++ opencare_frontend_nextjs/app/Components/Common/Navbar.jsx
@@ -1,6 +1,11 @@
+"use client"
+import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 export default function Navbar () {
+    const pathname = usePathname();
+
     return (
         <header class="w-full mt-5 text-gray-700 bg-white border-t border-gray-100 shadow-sm body-font">
         <div class="container flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
@@ -21,10 +26,10 @@ export default function Navbar () {
         />
             </a>
             <nav class="flex flex-wrap items-center justify-center pl-24 text-base md:ml-auto md:mr-auto">
-                <a href="/" class="mr-5 font-medium hover:text-white hover:bg-gray-500 bg-black text-white p-2">Home</a>
-                <a href="/hospitals" class="mr-5 font-medium hover:text-white hover:bg-gray-500 p-2">Hospitals</a>
-                <a href="/doctors" class="mr-5 font-medium hover:text-white hover:bg-gray-500 p-2">Doctors</a>
-                <a href="/institutes" class="font-medium hover:text-white hover:bg-gray-500 p-2">Institutes</a>
+                <Link href="/" className={`mr-5 font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/' ? 'bg-black text-white' : 'bg-red'}`}>Home</Link>
+                <Link href="/hospitals" className={`mr-5 font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/hospitals' ? 'bg-black text-white' : ''}`}>Hospitals</Link>
+                <Link href="/doctors" className={`mr-5 font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/doctors' ? 'bg-black text-white' : ''}`}>Doctors</Link>
+                <Link href="/institutes" className={`font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/institutes' ? 'bg-black text-white' : ''}`}>Institutes</Link>
             </nav>
             <div class="items-center h-full">
                 <a href="#_" class="mr-5 font-medium hover:text-gray-900">Login</a>
