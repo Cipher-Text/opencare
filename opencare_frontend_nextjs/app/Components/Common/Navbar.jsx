4ee32f81a6ea99c1c741ed20ebe78ec94bac7139
snehadevi <snehadevi2016105@gmail.com>
2023-09-15 17:26:24 +0600

    small fixes

--- opencare_frontend_nextjs/app/Components/Common/Navbar.jsx
+++ opencare_frontend_nextjs/app/Components/Common/Navbar.jsx
@@ -7,9 +7,9 @@ export default function Navbar () {
     const pathname = usePathname();
 
     return (
-        <header class="w-full mt-5 text-gray-700 bg-white border-t border-gray-100 shadow-sm body-font">
-        <div class="container flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
-            <a class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+        <header className="w-full mt-5 text-gray-700 bg-white border-t border-gray-100 shadow-sm body-font">
+        <div className="container flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
+            <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
             <Image
             src="/logo.svg"
             alt="Next.js Logo"
@@ -25,16 +25,16 @@ export default function Navbar () {
             priority
         />
             </a>
-            <nav class="flex flex-wrap items-center justify-center pl-24 text-base md:ml-auto md:mr-auto">
+            <nav className="flex flex-wrap items-center justify-center pl-24 text-base md:ml-auto md:mr-auto">
                 <Link href="/" className={`mr-5 font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/' ? 'bg-black text-white' : 'bg-red'}`}>Home</Link>
                 <Link href="/hospitals" className={`mr-5 font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/hospitals' ? 'bg-black text-white' : ''}`}>Hospitals</Link>
                 <Link href="/doctors" className={`mr-5 font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/doctors' ? 'bg-black text-white' : ''}`}>Doctors</Link>
                 <Link href="/institutes" className={`font-medium hover:text-white hover:bg-gray-500 p-2 ${pathname === '/institutes' ? 'bg-black text-white' : ''}`}>Institutes</Link>
             </nav>
-            <div class="items-center h-full">
-                <a href="#_" class="mr-5 font-medium hover:text-gray-900">Login</a>
+            <div className="items-center h-full">
+                <a href="#_" className="mr-5 font-medium hover:text-gray-900">Login</a>
                 <a href="#_"
-                    class="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-teal-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease">
+                    className="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-teal-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease">
                     Sign Up
                 </a>
             </div>
