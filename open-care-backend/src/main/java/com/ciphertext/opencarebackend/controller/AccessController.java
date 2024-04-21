a41a519f05560e077355b35f664c225fcadbbbff
Sadman Sobhan <imran110219@gmail.com>
2024-04-22 02:00:25 +0600

    add hospital multi filter

--- open-care-backend/src/main/java/com/ciphertext/opencarebackend/controller/AccessController.java
+++ open-care-backend/src/main/java/com/ciphertext/opencarebackend/controller/AccessController.java
@@ -26,6 +26,7 @@ public class AccessController {
     }
 
     @GetMapping("permission")
+    @ADMIN
     public ResponseEntity<?> getRolePermission() {
         return ResponseEntity.ok(accessManageService.getRolePermission());
     }
